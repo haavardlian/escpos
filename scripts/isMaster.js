71eda50320bf47e7985c89d5671e7fe8d4885b09
Håvard Lian <haavard@64bit.no>
2017-02-06 20:35:40 +0100

    Add output message on fail

--- scripts/isMaster.js
+++ scripts/isMaster.js
@@ -1,6 +1,9 @@
 var exec = require('child_process').exec;
 
 exec('git rev-parse --abbrev-ref HEAD', function (error, branch) {
-    if (branch == "master") process.exit(0); 
-    else process.exit(1);
+    if (branch == "master") process.exit(0);
+    else {
+        console.log("Branch is not master, failing....");
+        process.exit(1);
+    }
 });
