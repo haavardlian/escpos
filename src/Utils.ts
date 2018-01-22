a488f854271f8a6d85c4834817fc1ef0b2570a00
Håvard Lian <haavard@64bit.no>
2018-01-22 19:38:40 +0100

    Fix http/https image fetching

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,5 @@
 import { createReadStream } from "fs";
+import http = require("http");
 import https = require("https");
 
 const OK = 200;
@@ -12,13 +13,31 @@ export async function createStreamFromPath(path: string): Promise<NodeJS.Readabl
 }
 
 export function getRequestStream(address: string): Promise<NodeJS.ReadableStream> {
+    return address.startsWith("https")
+        ? getRequestStreamHttps(address)
+        : getRequestStreamHttp(address);
+}
+
+function getRequestStreamHttp(address: string): Promise<NodeJS.ReadableStream> {
     return new Promise((resolve, reject) => {
-        const request = https.get(address, (response) => {
+        const request = http.get(address, (response: http.IncomingMessage) => {
             if (response.statusCode !== OK) {
                 reject(new Error("Request failed, status code: " + response.statusCode));
             }
             resolve(response);
         });
-        request.on("error", (err) => reject(err));
+        request.on("error", (err: Error) => reject(err));
+    });
+}
+
+function getRequestStreamHttps(address: string): Promise<NodeJS.ReadableStream> {
+    return new Promise((resolve, reject) => {
+        const request = https.get(address, (response: http.IncomingMessage) => {
+            if (response.statusCode !== OK) {
+                reject(new Error("Request failed, status code: " + response.statusCode));
+            }
+            resolve(response);
+        });
+        request.on("error", (err: Error) => reject(err));
     });
 }
