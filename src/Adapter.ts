689d268396fde523facf9a0895de460baf98a917
Jay Sale <Jay.Sale@zapper.com>
2018-03-13 10:29:45 +0200

    checkin

--- src/Adapter.ts
+++ src/Adapter.ts
@@ -2,7 +2,7 @@ abstract class Adapter {
     public abstract open(): Promise<void>;
     public abstract write(data: Uint8Array): Promise<void>;
     public abstract close(): Promise<void>;
-    protected onDataReceived: (data: any) => void
+    public onDataReceived: (data: any) => void
 }
 
 export default Adapter;
